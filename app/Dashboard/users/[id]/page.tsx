b8eb139a736352b7f697a28bdd324c4e415e251c
Fariha Promi <farihapromi2611@gmail.com>
2025-11-21 20:25:41 +0600

    Add dashboard layout

--- app/Dashboard/users/[id]/page.tsx
+++ app/Dashboard/users/[id]/page.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 
-const page = () => {
-  return <div>User details</div>;
+const page = async ({ params }: { params: Promise<{ id: string }> }) => {
+  const { id } = await params;
+  return <div>User details :user {id}</div>;
 };
 
 export default page;
